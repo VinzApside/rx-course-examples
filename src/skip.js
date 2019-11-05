39c825c09be79f275ae92f8a27a26cce28b2cf37
Vinz <ballutvincent@gmail.com>
2019-11-05 21:13:54 +0100

    use skipwhile and skipuntil

--- src/skip.js
+++ src/skip.js
@@ -4,15 +4,33 @@ import {
 
 import {
     from,
-    fromEvent
+    fromEvent,
+    interval
 } from "rxjs";
 
 import {
+    take,
     skip,
     skipLast,
-    skipWhile
+    skipWhile,
+    skipUntil
 } from 'rxjs/operators';
 
 // from(["apple", "grapes", "oranges", "pears"]).pipe(skip(2)).subscribe(add.li);
 
-from(["apple", "grapes", "oranges", "pears"]).pipe(skipLast(2)).subscribe(add.li);
+// from(["apple", "grapes", "oranges", "pears"]).pipe(skipLast(2)).subscribe(add.li);
+
+// interval(1000).pipe(
+//     take(10),
+//     skipWhile(
+//         (number) => number < 2)
+// ).subscribe(add.li);
+
+const button = document.getElementById('submit');
+const buttonEvents = fromEvent(button, 'click');
+
+interval(1000).pipe(
+    skipUntil(buttonEvents)
+).subscribe(add.li);
+
+//click on button to show li
