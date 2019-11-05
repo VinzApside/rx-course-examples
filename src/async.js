f7640180f23bcb560377ca913423b5a3e5a8206f
Vinz <vincentballut@outlook.fr>
2019-11-05 10:23:58 +0000

    1st example

--- src/async.js
+++ src/async.js
@@ -1,1 +1,20 @@
-import { add } from './helpers';
+import { add } from "./helpers";
+
+add.li("line 3");
+
+function callback(message) {
+  add.li(message);
+}
+callback("hi");
+
+function greeting(message, cb) {
+  let start = Date.now();
+  for (let i = 0; i < 10000000000; i++) {
+    //do nothing
+  }
+  add.li(`took: ${Date.now() - start} ms`);
+  cb(message);
+}
+greeting("hello from here", callback);
+
+add.li("line 5");
