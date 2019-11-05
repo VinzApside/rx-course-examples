a92ee50ad129ea3e4a430ab16f3bcceb774d2c3b
Vinz <vincentballut@outlook.fr>
2019-11-05 14:54:14 +0000

    use fromEvent

--- src/from-of.js
+++ src/from-of.js
@@ -1,5 +1,10 @@
 import { add } from "./helpers";
-import { of, from } from "rxjs";
+import { of, from, fromEvent } from "rxjs";
+
+const submit = document.getElementById("submit");
+const clicks = fromEvent(submit, "click").subscribe(event => {
+  add.li("clicked !");
+});
 
 // of = for list
 const numbers = of(1, 2, 3, 4).subscribe(add.li);
