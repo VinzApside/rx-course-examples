8701e577cc95578a845a56fcca290a74a88f650d
Vinz <vincentballut@outlook.fr>
2019-11-05 14:18:03 +0000

    unsubscribe

--- src/async.js
+++ src/async.js
@@ -2,10 +2,8 @@ import { add } from "./helpers";
 import { Observable } from "rxjs";
 
 const o = new Observable(observer => {
-  setTimeout(() => {
+  setInterval(() => {
     observer.next("We are complete observable !");
-    observer.next("We are complete observable !");
-    observer.complete();
   }, 1000);
 });
 
@@ -21,10 +19,14 @@ p.then(message => {
 });
 
 //Observable
-o.subscribe({
+const subscription = o.subscribe({
   next: message => {
     add.li(message);
   },
   error: error => console.log(error),
   complete: () => add.li("complete")
 });
+
+setTimeout(() => {
+  subscription.unsubscribe();
+}, 3000);
