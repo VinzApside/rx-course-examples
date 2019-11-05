76e7da013ba1896bad83c488084e5e3002c86178
Vinz <vincentballut@outlook.fr>
2019-11-05 10:28:45 +0000

    2nd example

--- src/async.js
+++ src/async.js
@@ -2,19 +2,12 @@ import { add } from "./helpers";
 
 add.li("line 3");
 
-function callback(message) {
-  add.li(message);
-}
-callback("hi");
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("We are complete !");
+  }, 5000);
+});
 
-function greeting(message, cb) {
-  let start = Date.now();
-  for (let i = 0; i < 10000000000; i++) {
-    //do nothing
-  }
-  add.li(`took: ${Date.now() - start} ms`);
-  cb(message);
-}
-greeting("hello from here", callback);
+p.then(message => add.li(message));
 
-add.li("line 5");
+add.li("line 12");
