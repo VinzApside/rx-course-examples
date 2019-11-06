6c495d599f452e890b0d85e82b474ae6f18d2967
Vinz <vincentballut@outlook.fr>
2019-11-06 09:45:45 +0000

    use reduce

--- src/scan-reduce.js
+++ src/scan-reduce.js
@@ -4,10 +4,27 @@ import { scan, reduce, take, concatMap } from "rxjs/operators";
 
 import { interval, from } from "rxjs";
 
+//create finobacci sequence show each sequence
+// const FS = interval(1000)
+//   .pipe(
+//     take(10),
+//     scan(
+//       (acc, value) => {
+//         const n = value + 1;
+//         const last = acc[n];
+//         const beforeLast = acc[n - 1];
+//         return [...acc, last + beforeLast];
+//       },
+//       [0, 1]
+//     )
+//   )
+//   .subscribe(add.li);
+
+//create finobacci sequence show final sequence only
 const FS = interval(1000)
   .pipe(
-    take(10),
-    scan(
+    take(20),
+    reduce(
       (acc, value) => {
         const n = value + 1;
         const last = acc[n];
