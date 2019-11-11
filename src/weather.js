d6d2b35295b30af72f5cd0564296cbd61acb2758
Vinz <ballutvincent@gmail.com>
2019-11-11 11:51:11 +0100

    install eslint and create our import

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,23 @@
-
-import {  } from "rxjs";
+import {
+    fromEvent,
+    BehaviorSubject,
+    Subject
+} from "rxjs";
 import {} from "rxjs/operators";
-import { add } from "./helpers";
+import {
+    add
+} from "./helpers";
+
+
+//handles to our elements
+const searchBox = document.getElementById('search');
+const resultsBox = document.getElementById('results-container')
+
+//event handlers
+const searchEvent = fromEvent(searchBox, 'keyUp');
+const resultsEvent = fromEvent(resultsBox, 'click');
+
+//subjects
+const inputSubject = new BehaviorSubject('');
+const placeSubject = new Subject();
+const weatherSubject = new Subject();
