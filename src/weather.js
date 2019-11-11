0dd30b55e49532a48f856318035b77eed8aaa779
Vinz <ballutvincent@gmail.com>
2019-11-11 18:53:42 +0100

    get place result

--- src/weather.js
+++ src/weather.js
@@ -1,44 +1,47 @@
-import {
-    fromEvent,
-    BehaviorSubject,
-    Subject
-} from "rxjs";
-import {
-    debounceTime,
-    switchMap
-} from "rxjs/operators";
-import {
-    add
-} from "./helpers";
-import {
-    ajax
-} from "rxjs/ajax";
-
+import { fromEvent, BehaviorSubject, Subject, from } from "rxjs";
+import { debounceTime, switchMap, tap, skip } from "rxjs/operators";
+import { add } from "./helpers";
+import { ajax } from "rxjs/ajax";
 
 //handles to our elements
-const searchBox = document.getElementById('search');
-const resultsBox = document.getElementById('results-container');
-const spinner = document.getElementById('spinner');
+const searchBox = document.getElementById("search");
+const resultsBox = document.getElementById("results-container");
+const spinner = document.getElementById("spinner");
 
 //event handlers
-const searchEvent = fromEvent(searchBox, 'keyUp');
-const resultsEvent = fromEvent(resultsBox, 'click');
+const searchEvent = fromEvent(searchBox, "keyUp");
+const resultsEvent = fromEvent(resultsBox, "click");
 
 //subjects
-const inputSubject = new BehaviorSubject('');
+const inputSubject = new BehaviorSubject("");
 const placeSubject = new Subject();
 const weatherSubject = new Subject();
 
-inputSubject.pipe(
-    tap(
-        () => {
-            spinner.className = "spinner";
-        }
-    ),
+inputSubject
+  .pipe(
+    skip(1),
+    tap(() => {
+      spinner.className = "spinner";
+      console.log("+++");
+    }),
     debounceTime(1000),
-    switchMap(
-        searchTerm => {
-            return ajax.getJSON('http://localhost:300/autocomplente/${searchTerm}')
-        }
-    )
-);
+    switchMap(searchTerm => {
+      return ajax
+        .getJSON(`http://localhost:3000/autocomplete/${searchTerm}`)
+        .pipe(
+          tap(() => {
+            spinner.className = "";
+          }),
+          switchMap(results => {
+            return from(results);
+          })
+        );
+    })
+  )
+  .subscribe(result => {
+    add.result(result.description, result.place_id);
+  });
+
+searchEvent.subscribe(event => {
+  inputSubject.next(searchBox.value);
+});
