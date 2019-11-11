798fbd2924903ec1cfa1bc0e998e116ef86c0a09
Vinz <ballutvincent@gmail.com>
2019-11-11 20:07:33 +0100

    render result
    not finish because FRONT work

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,17 @@
-import { fromEvent, BehaviorSubject, Subject, from } from "rxjs";
-import { debounceTime, switchMap, tap, skip, pluck } from "rxjs/operators";
+import { fromEvent, BehaviorSubject, Subject, from, combineLatest } from "rxjs";
+import {
+  debounceTime,
+  switchMap,
+  tap,
+  skip,
+  pluck,
+  skipWhile
+} from "rxjs/operators";
 import { add } from "./helpers";
 import { ajax } from "rxjs/ajax";
+
+const lastSearch = localStorage.getItem("lastSearch");
+const firstTerm = lastSearch !== undefined ? lastSearch : undefined;
 
 //handles to our elements
 const searchBox = document.getElementById("search");
@@ -13,16 +23,16 @@ const searchEvent = fromEvent(searchBox, "keyUp");
 const resultsEvent = fromEvent(resultsBox, "click");
 
 //subjects
-const inputSubject = new BehaviorSubject("");
+const inputSubject = new BehaviorSubject(firstTerm);
 const placeSubject = new Subject();
 const weatherSubject = new Subject();
 
 const inputData = inputSubject
   .pipe(
-    skip(1),
+    skipWhile(value => value === null || value.length < 3),
     tap(() => {
       spinner.className = "spinner";
-      console.log("+++");
+      resultsBox.innerHTML = "";
     }),
     debounceTime(1000),
     switchMap(searchTerm => {
@@ -39,6 +49,7 @@ const inputData = inputSubject
     })
   )
   .subscribe(result => {
+    localStorage.setItem("lastSearch", searchBox.value);
     add.result(result.description, result.place_id);
   });
 
@@ -58,18 +69,36 @@ const placeData = resultsEvent
     placeSubject.next(place);
   });
 
-const weathData = placeSubject
-  .pipe(
-    tap(place => {
-      console.log(place);
-    }),
-    pluck("geometry", "location"),
-    switchMap(coords => {
-      return ajax
-        .getJSON(`http://localhost:3000/weather/${coords.lat}/${coords.lng}`)
-        .pipe(pluck("currently"));
-    })
-  )
-  .subscribe(stream => {
-    console.log(stream);
-  });
+const weatherData = placeSubject.pipe(
+  // tap(place => {
+  //   console.log(place);
+  // }),
+  pluck("geometry", "location"),
+  switchMap(coords => {
+    return ajax
+      .getJSON(`http://localhost:3000/weather/${coords.lat}/${coords.lng}`)
+      .pipe(pluck("currently"));
+  })
+);
+// .subscribe(stream => {
+//   console.log(stream);
+// });
+
+combineLatest(weatherData, placeSubject).subscribe(result => {
+  const weather = result[0];
+  const place = result[1];
+
+  document.getElementById("image-container").innerHTML = "";
+
+  if (place.photo !== undefined) {
+    add.div(``);
+  } else {
+    add.div(
+      `<div>
+    <p>feel like : ${Math.round(weather.apparentTemperature)}&deg</p>
+    <p>Current conditions: ${weather.summary}</p>
+    <p>Chance of rain : ${weather.precipProbability}</p>
+    </div>`
+    );
+  }
+});
